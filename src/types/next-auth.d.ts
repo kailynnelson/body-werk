c7b81babb3888d76afd097052e8c7ceeab57fa31
Kailyn <nelsonkailynr@gmail.com>
2025-05-23 13:56:43 -0700

    Fix NextAuth type imports

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,5 +1,5 @@
-import NextAuth, { DefaultSession } from "next-auth";
-import { JWT as NextAuthJWT } from "next-auth/jwt";
+import type { DefaultSession } from "next-auth";
+import type { JWT as NextAuthJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
