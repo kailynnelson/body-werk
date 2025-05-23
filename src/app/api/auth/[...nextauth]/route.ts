d8a651f7431865e73a18da7668d4525545af59af
Kailyn <nelsonkailynr@gmail.com>
2025-05-23 14:29:01 -0700

    fix: remove unused DefaultSession import to resolve ESLint error #1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { JWT } from "next-auth/jwt";
 
